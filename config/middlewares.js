1f20950a3efb7a1971b38d5e2f3e9d8e30a8e156
Ivan <iv@Ivans-MacBook-Air.local>
2022-11-23 01:24:58 +0300

    fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,5 @@
-module.exports = ({ env }) => [
+module.exports = [
   "strapi::errors",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::favicon",
-  "strapi::public",
   {
     name: "strapi::security",
     config: {
@@ -14,21 +7,18 @@ module.exports = ({ env }) => [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
-          ],
+          "img-src": ["'self'", "data:", "blob:", "*.digitaloceanspaces.com"],
+          "media-src": ["'self'", "data:", "blob:"],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
 ];
